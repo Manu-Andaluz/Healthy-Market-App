735807cf9b70417d1ef81724ab1fdc7518f288a9
manu-andaluz <manupeandaluz@gmail.com>
2023-02-14 16:01:58 -0300

    agregando carpeta de models y routes

--- server/index.js
+++ server/index.js
@@ -18,17 +18,17 @@ app.get("/", (req, res) => {
     res.send("Welcome our to online shop API...");
 });
 
-// const uri = process.env.DB_URI
+const uri = process.env.DB_URI
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server running on port: ${port}...`);
 });
 
-// mongoose
-//     .connect(uri, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//     })
-//     .then(() => console.log("MongoDB connection established..."))
-//     .catch((error) => console.error("MongoDB connection failed:", error.message));
+mongoose
+    .connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB connection established..."))
+    .catch((error) => console.error("MongoDB connection failed:", error.message));
