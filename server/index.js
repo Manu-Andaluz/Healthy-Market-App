1087f782a79299c0ec771e9b74b8100d31091930
manu-andaluz <manupeandaluz@gmail.com>
2023-02-16 11:38:03 -0300

    no subir commit

--- server/index.js
+++ server/index.js
@@ -2,16 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const mainRouter = require("./routes/index");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     next();
 });
 
@@ -19,7 +23,7 @@ app.get("/", (req, res) => {
     res.send("Welcome our to online shop API...");
 });
 
-const uri = process.env.DB_URI
+const uri = process.env.DB_URI;
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
@@ -33,3 +37,5 @@ mongoose
     })
     .then(() => console.log("MongoDB connection established..."))
     .catch((error) => console.error("MongoDB connection failed:", error.message));
+
+app.use(mainRouter);
