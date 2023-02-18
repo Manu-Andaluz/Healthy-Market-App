1937b3fded403842076c01a8b6a0ae3b9b40f7a0
Rocio Molina <rociomolina.b3@gmail.com>
2023-02-18 12:54:23 -0300

    config de tilwind

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
