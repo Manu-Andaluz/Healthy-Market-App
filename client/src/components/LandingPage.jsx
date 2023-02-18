c422d0dcd02d139aae5e143694355f2e7eaadece
loreleymilazzo <clm12343@gmail.com>
2023-02-18 20:19:52 -0300

    agregue texto y boton

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -51,10 +51,10 @@ const LandingPage = () => {
           </ul>
         </div>
         <div className="flex items-center lg:ml-auto">
-          <button type="button" class="inline-block px-6 py-2.5 mr-2 bg-transparent text-green1 font-medium text-xs leading-tight uppercase rounded hover:text-grisLetter hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light">  Quiero sumar mi tienda</button>
+          <button type="button" class="inline-block px-6 py-2.5 mr-2 bg-transparent text-green1 font-bree text-xs leading-tight uppercase rounded hover:text-grisLetter hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light">  Quiero sumar mi tienda</button>
 
-          <button type="button" class="inline-block px-6 py-2.5 bg-green1 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green2 hover:shadow-lg focus:bg-grisLetter focus:shadow-lg focus:outline-none focus:ring-0 active:bg-rosa active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light">Mi perfil </button>
-          <button type="button" class="inline-block px-6 py-2.5 mr-2 bg-transparent text-green1 font-medium text-xs leading-tight uppercase rounded hover:text-grisLetter hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light">    Sobre nosotros</button>
+          <button type="button" class="inline-block px-6 py-2.5 bg-green1 text-white font-bree text-xs leading-tight uppercase rounded shadow-md hover:bg-green2 hover:shadow-lg focus:bg-grisLetter focus:shadow-lg focus:outline-none focus:ring-0 active:bg-rosa active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light">Mi perfil </button>
+          <button type="button" class="inline-block px-6 py-2.5 mr-2 bg-transparent text-green1 font-bree text-xs leading-tight uppercase rounded hover:text-grisLetter hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light">    Sobre nosotros</button>
         </div>
       </div>
     </nav>
@@ -63,11 +63,11 @@ const LandingPage = () => {
       <div className="container mx-auto xl:px-32">
         <div className="grid lg:grid-cols-2 gap-12 flex items-center">
           <div className="mt-12 lg:mt-0">
-            <h1 className="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">Healthy Markey <br /><span class="text-">APP</span></h1><p className="text-grisLetter text-sm md:text-base mb-8" style={{ fontSize: '16px' }}>
+            <h1 className="text-5xl md:text-6xl xl:text-7xl font-continuo tracking-tight mb-12">Healthy Market <br /><span class="text-">APP</span></h1><p className="text-grisLetter font-bree text-sm md:text-base mb-8" style={{ fontSize: '17px' }}>
             Bienvenido a Healthy Market App, el lugar perfecto para encontrar y comprar alimentos saludables de alta calidad. Con nuestra plataforma, podrás apoyar a comerciantes locales y encontrar fácilmente productos orgánicos, sin gluten, vegetarianos y veganos. ¿Estás listo para comenzar tu viaje hacia una alimentación saludable? Regístrate hoy mismo en Healthy Market App. ¡Te esperamos!
               </p>
-            <a className="inline-block px-7 py-3 mr-2 bg-green1 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-green2 hover:shadow-lg focus:bg-grisLetter focus:shadow-lg focus:outline-none focus:ring-0 active:bg-grisLetter active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">Ir a la tienda</a>
-            <a className="inline-block px-7 py-3 bg-transparent text-green1 font-medium text-sm leading-snug uppercase rounded hover:text-grisLetter hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">Ver ofertas</a>
+            <a className="inline-block px-7 py-3 mr-2 bg-green1 text-white font-bree text-sm leading-snug uppercase rounded shadow-md hover:bg-green2 hover:shadow-lg focus:bg-grisLetter focus:shadow-lg focus:outline-none focus:ring-0 active:bg-grisLetter active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">Ir a la tienda</a>
+            <a className="inline-block px-7 py-3 bg-transparent text-green1 font-bree text-sm leading-snug uppercase rounded hover:text-grisLetter hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">Ver ofertas</a>
           </div>
           <div className="mb-12 lg:mb-0">
             <img
