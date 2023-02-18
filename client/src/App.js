27b2720167b7f4c254f9e2d39bcec5f85451a93d
gastongg13 <gastongg13@gmail.com>
2023-02-18 19:06:59 -0300

    Adding/About/ReconfigTailFonts

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
-import {BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar"
-import LandingPage from "./components/LandingPage"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import LandingPage from "./components/LandingPage";
 import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-      <Routes>
-      <Route path="/" element={<LandingPage/>} />
-      <Route path="/home" element={<Home/>}/>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
