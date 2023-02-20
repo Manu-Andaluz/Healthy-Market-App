814f16b3070aaef4a635204f336eaf5b131c48bd
manu-andaluz <manupeandaluz@gmail.com>
2023-02-20 19:16:12 -0300

    creating register form

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar";
 import LandingPage from "./components/LandingPage";
 import Home from "./components/Home";
 import About from "./components/About";
+import RegisterForm from "./components/RegisterForm";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/register" element={<RegisterForm />} />
         </Routes>
       </BrowserRouter>
     </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,23 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      white: "#ffffff",
-      green1: "#606C38",
-      green2: "#A1C181",
-      crema: "#FEFAE0",
-      beige: "#DDA15E",
-      rosa: "#FAA275",
-      amarillo:"#F9E79F",
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#ffffff",
+        green1: "#606C38",
+        green2: "#A1C181",
+        crema: "#FEFAE0",
+        beige: "#DDA15E",
+        rosa: "#FAA275",
+        amarillo: "#F9E79F",
+      },
     },
     fontFamily: {
       continuo: ["Continuo", "sans-serif"],
       bree: ["bree", "sans-serif"],
     },
-   
-  plugins: [require("flowbite/plugin")],
-}
-}
+
+    plugins: [require("flowbite/plugin")],
+  },
+};
