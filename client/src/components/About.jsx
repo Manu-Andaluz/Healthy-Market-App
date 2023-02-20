c4d8e367acee80f0c339770248e6ae8e4d968a79
Rocio Molina <rociomolina.b3@gmail.com>
2023-02-19 21:52:15 -0300

    Links home-landing landing-about

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import  NavBar from "./NavBar";
 
 const About = () => {
   return (
-    <div className="">
+    <div className="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4">
       <NavBar/>
       <div className="flex flex-col lg:flex-row justify-between gap-8">
         <div className="w-full lg:w-5/12 flex flex-col justify-center">
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -12,17 +12,17 @@ export default function NavBar(){
     
     return(
         <nav className='bg-green2 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900'>
-            <div className="container flex flex-wrap items-center justify-between mx-auto">
-            <a href="https://flowbite.com/" className="flex items-center">
+            <div className="container flex flex-wrap items-center mx-auto">
+            <Link to='/'>
             <img
                 src={logo}
                 className="h-14 pr-4 text-4xl"
                 alt="Flowbite Logo"
-              />
+              /></Link>
               <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white font-continuo">
                 Healthy market app
               </span>
-            </a>
+              
             
             </div>
 
