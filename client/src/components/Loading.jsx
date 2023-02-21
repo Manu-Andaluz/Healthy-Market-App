3c255ee5715a5f802f7984bc19fbddbd66e73957
Your Name~ <you@example.com>
2023-02-21 16:44:35 -0300

    load

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import food from "../pictures/food.gif"
+
+
+
+
+export default function Loading(){
+    return(
+        <div className=''>
+            <div className=''>
+            <img className='object-center bg-white-300 min-h-0' src={food} alt="Loading"  />
+            </div>
+        </div>
+    )
+}
