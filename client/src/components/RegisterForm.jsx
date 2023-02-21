ef63148edc44545155428409df46ba7e90d6b047
gastongg13 <gastongg13@gmail.com>
2023-02-21 17:56:05 -0300

    Auth/Login/Controllers

--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -1,94 +1,141 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 const RegisterForm = () => {
+  const [form, setForm] = useState({
+    name: "",
+    surname: "",
+    birthday: "",
+    nationality: "",
+    adress: { zip: "", city: "", adress: "" },
+    email: "",
+    password: "",
+  });
+
+  useEffect(() => {
+    console.log(form);
+  }, [form]);
+
+  const changeHandler = (e) => {
+    if (
+      e.target.name === "zip" ||
+      e.target.name === "city" ||
+      e.target.name === "adress"
+    ) {
+      setForm({
+        ...form,
+        adress: { ...form.adress, [e.target.name]: e.target.value },
+      });
+    } else {
+      setForm({ ...form, [e.target.name]: e.target.value });
+    }
+    e.preventDefault();
+    //console.log(form);
+  };
+
+  const handleOnClick = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="grid text-center h-screen content-center gap-5 items-center">
       <h2 className="text-3xl font-bold">Crear Cuenta</h2>
       <div className="flex justify-center items-center bg-blue">
-        <form class="w-full max-w-lg p-2">
-          <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+        <form className="w-full max-w-lg p-2">
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
               <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-first-name"
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-first-name"
               >
                 Nombre
               </label>
               <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                id="grid-first-name"
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                 type="text"
                 placeholder="Jane"
+                name="name"
+                value={form.name}
+                onChange={changeHandler}
               />
             </div>
-            <div class="w-full md:w-1/2 px-3">
+            <div className="w-full md:w-1/2 px-3">
               <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-last-name"
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-last-name"
               >
                 Apellido
               </label>
               <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-last-name"
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 type="text"
                 placeholder="Doe"
+                name="surname"
+                value={form.surname}
+                onChange={changeHandler}
               />
             </div>
           </div>
-          <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full px-3">
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
               <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-password"
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-password"
               >
                 Email
               </label>
               <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-password"
+                name="email"
+                value={form.email}
+                onChange={changeHandler}
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 type="email"
                 placeholder="jane@email.com"
               />
             </div>
           </div>
-          <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full px-3">
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
               <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-password"
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-password"
               >
                 Contraseña
               </label>
               <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-password"
+                name="password"
+                value={form.password}
+                onChange={changeHandler}
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 type="password"
                 placeholder="******************"
               />
             </div>
           </div>
-          <div class="flex flex-wrap -mx-3 mb-2 py-2">
-            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+          <div className="flex flex-wrap -mx-3 mb-2 py-2">
+            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
               <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-state"
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-state"
               >
                 Pais
               </label>
-              <div class="relative">
+              <div className="relative">
                 <select
-                  class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-state"
+                  className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  onChange={changeHandler}
+                  name="nationality"
                 >
-                  <option>Argentina</option>
-                  <option>México</option>
-                  <option>Colombia</option>
-                  <option>Chile</option>
+                  <option value="">Páis</option>
+                  <option value="Argentina">Argentina</option>
+                  <option value="México">México</option>
+                  <option value="Colombia">Colombia</option>
+                  <option value="Chile">Chile</option>
                 </select>
-                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                   <svg
-                    class="fill-current h-4 w-4"
+                    className="fill-current h-4 w-4"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                   >
@@ -97,67 +144,75 @@ const RegisterForm = () => {
                 </div>
               </div>
             </div>
-            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
               <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-city"
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-city"
               >
                 Ciudad
               </label>
               <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-city"
+                name="city"
+                value={form.adress.city}
+                onChange={changeHandler}
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 type="text"
                 placeholder="Córdoba"
               />
             </div>
-            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
               <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-zip"
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-zip"
               >
                 Código Postal
               </label>
               <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-zip"
+                name="zip"
+                value={form.adress.zip}
+                onChange={changeHandler}
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 type="text"
                 placeholder="5501"
               />
             </div>
           </div>
-          <div class="flex flex-wrap justify-center -mx-3 mb-6 pt-2">
-            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <div className="flex flex-wrap justify-center -mx-3 mb-6 pt-2">
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
               <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-city"
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-city"
               >
                 Dirección
               </label>
               <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-city"
+                name="adress"
+                value={form.adress.adress}
+                onChange={changeHandler}
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 type="text"
                 placeholder="Av. General Alvear 550"
               />
             </div>
-            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0 mx-auto">
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0 mx-auto">
               <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-state"
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-state"
               >
                 Fecha de Nacimiento
               </label>
-              <div class="relative">
+              <div className="relative">
                 <input
                   type="date"
                   placeholder="+18"
-                  class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-state"
+                  name="birthday"
+                  value={form.birthday}
+                  onChange={changeHandler}
+                  className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 />
-                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                   <svg
-                    class="fill-current h-4 w-4"
+                    className="fill-current h-4 w-4"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                   ></svg>
@@ -167,7 +222,10 @@ const RegisterForm = () => {
           </div>
           <div className="flex m-auto justify-center items-center py-5 gap-4">
             <Link to="/login">Iniciar Sessión</Link>
-            <button class=" bg-green1 hover:bg-hoverGreen1 text-white font-bold py-2 px-4 rounded">
+            <button
+              onClick={changeHandler}
+              className=" bg-green1 hover:bg-hoverGreen1 text-white font-bold py-2 px-4 rounded"
+            >
               Registrarse
             </button>
           </div>
