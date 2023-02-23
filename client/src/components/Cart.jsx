16bcf86294f230560775e9c2e2640877a5f84c38
Rocio Molina <rociomolina.b3@gmail.com>
2023-02-23 10:33:27 -0300

    Datail of product, and CartSlice

---
+++ client/src/components/Cart.jsx
@@ -0,0 +1,44 @@
+
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addToCart,
+  clearCart,
+  decreaseCart,
+  getTotals,
+  removeFromCart,
+} from "../slices/cartSlice";
+
+import { Link } from "react-router-dom";
+
+
+const Cart = () => {
+  const cart = useSelector((state) => state.cart);
+  const user = useSelector((state) => state.user);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(getTotals());
+  }, [cart, dispatch]);
+
+  const handleAddToCart = (product) => {
+    dispatch(addToCart(product));
+  };
+  const handleDecreaseCart = (product) => {
+    dispatch(decreaseCart(product));
+  };
+  const handleRemoveFromCart = (product) => {
+    dispatch(removeFromCart(product));
+  };
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
+}
+
+return(<>
+
+
+</>);
