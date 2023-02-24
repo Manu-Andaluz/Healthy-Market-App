2c5ca3b99a6afb4fffdd959bc7c2ac4df3df9897
manu-andaluz <manupeandaluz@gmail.com>
2023-02-24 12:53:04 -0300

    fixing erros

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="p-4 mt-5 bg-amarillo rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+    <footer className="p-4 mt-5 bg-amarillo shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
       <span className="text-xs text-gray-500 sm:text-center dark:text-gray-400">
         © 2023{" "}
         <a href="https://flowbite.com/" className="hover:underline ">
