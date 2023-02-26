90e1778ecb2a72eaff6e62a376729068fd0902e5
manu-andaluz <manupeandaluz@gmail.com>
2023-02-26 00:24:05 -0300

    añadiendo estilos a las cards

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -18,15 +18,17 @@ const Cards = (currentProduct) => {
   const handleOnClick = (item) => {
     dispatch(addToCart(item));
     navigate("/cart");
-    console.log("handleSubmit");
   };
 
   return (
     <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 px-5 mt-3">
-      {currentProduct.currentProduct &&
+      {currentProduct &&
         currentProduct.currentProduct.map((product) => {
           return (
-            <div className="grid place-content-center text-center">
+            <div
+              className="grid place-content-center text-center gap-4"
+              key={product._id}
+            >
               <Link to={`/detail/${product._id}`}>
                 <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
                   <img
@@ -37,15 +39,15 @@ const Cards = (currentProduct) => {
                 </div>
               </Link>
 
-              <h3 className="mt-4 text-sm font-bree text-gray-700">
+              <h3 className="text-sm font-bree text-gray-700">
                 {product.name}
               </h3>
-              <p className="mt-4 text-lg font-bold text-gray-900">
+              <p className="text-lg font-bold text-gray-900">
                 ${product.price}
               </p>
               <button
                 onClick={() => handleOnClick(product)}
-                class="bg-yellow-300 opacity-75 hover:opacity-100 text-yellow-900 hover:text-gray-900 rounded-full mt-4 py-2 font-semibold"
+                className=" bg-yellow-400 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded w-fit mx-auto"
               >
                 <i class="mdi mdi-cart -ml-2 mr-2"></i> AGREGAR AL CARRITO{" "}
               </button>
