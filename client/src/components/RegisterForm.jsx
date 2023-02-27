7247ce81955657064c845b30d8fe4b1d490ff091
gastongg13 <gastongg13@gmail.com>
2023-02-27 12:28:15 -0300

    ToolTipMayus

--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -159,7 +159,7 @@ const RegisterForm = () => {
                   Apellido
                 </label>
                 <input
-                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                   type="text"
                   placeholder="Doe"
                   name="surname"
