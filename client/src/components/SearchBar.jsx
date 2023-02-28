24c0e50a0ee3caa20277ed66eb77f4dc61b2bba1
gastongg13 <gastongg13@gmail.com>
2023-02-28 16:08:39 -0300

    arreglandoFiltros

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -19,15 +19,15 @@ export default function SearchBar() {
       <form className="bg-white">
         <label
           for="default-search"
-          ="mb-2 text-sm font-medium text-gray-200 sr-only"
+          className="mb-2 text-sm font-medium text-gray-200 sr-only"
         >
           Search
         </label>
-        <div ="relative">
-          <div ="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <div className="relative">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <svg
               aria-hidden="true"
-              ="w-5 h-5 text-gray-500"
+              className="w-5 h-5 text-gray-500"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
@@ -47,7 +47,7 @@ export default function SearchBar() {
             onChange={onInputChange}
             value={search}
             id="default-search"
-            ="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+            className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
           />
         </div>
       </form>
