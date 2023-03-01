924a54bb58feb62bf34597a242e828f260301776
manu-andaluz <manupeandaluz@gmail.com>
2023-03-01 11:44:22 -0300

    linkeando admin dashboard en /admin

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,18 +1,28 @@
 const { Router } = require("express");
-const passport = require('passport');
+const passport = require("passport");
 const {
   getUsersController,
   registerController,
   loginController,
-  loginGoogle
+  loginGoogle,
 } = require("../controllers/userController");
 
 const userRouter = Router();
 
 userRouter.get("/", getUsersController);
 userRouter.post("/register", registerController);
-userRouter.post("/loggin", passport.authenticate('local', { session: false }), loginController);
-userRouter.get('/google', passport.authenticate('google', { session: false , failureRedirect: '/loggin'}), loginGoogle);
-
+userRouter.post(
+  "/loggin",
+  passport.authenticate("local", { session: false }),
+  loginController
+);
+userRouter.get(
+  "/google",
+  passport.authenticate("google", {
+    session: false,
+    failureRedirect: "/loggin",
+  }),
+  loginGoogle
+);
 
 module.exports = userRouter;
