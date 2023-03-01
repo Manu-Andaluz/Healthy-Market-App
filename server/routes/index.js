9f8d150f8b13ee7645b5ec34ebeed14a9f2d145d
manu-andaluz <manupeandaluz@gmail.com>
2023-03-01 00:43:54 -0300

    admin dashboard with admin.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ const userRouter = require("./userRouter");
 const productRouter = require("./productRouter");
 const companyRouter = require("./companyRouter");
 const oderRouter = require("./orderRouter");
+const adminRouter = require("./adminRouter");
 
 const mainRouter = Router();
 
@@ -10,5 +11,6 @@ mainRouter.use("/users", userRouter);
 mainRouter.use("/products", productRouter);
 mainRouter.use("/companies", companyRouter);
 mainRouter.use("/order", oderRouter);
+mainRouter.use("/admin", adminRouter);
 
 module.exports = mainRouter;
