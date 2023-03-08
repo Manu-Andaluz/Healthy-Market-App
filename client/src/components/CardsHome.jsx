c41541f0a4fd37ea7d7529c6acf59f2fd574da4c
gastongg13 <gastongg13@gmail.com>
2023-03-08 19:26:08 -0300

    arreglandoFiltros/ErroresDeConsola

--- client/src/components/CardsHome.jsx
+++ client/src/components/CardsHome.jsx
@@ -52,7 +52,7 @@ const CardHome = ({ products }) => {
                   onClick={() => handleOnClick(product)}
                   className=" bg-yellow-500 hover:bg-yellow-400 text-white font-bold py-2 px-4 rounded w-fit mx-auto"
                 >
-                  <i class="mdi mdi-cart -ml-2 mr-2"></i> AGREGAR AL CARRITO{" "}
+                  <i className="mdi mdi-cart -ml-2 mr-2"></i> AGREGAR AL CARRITO{" "}
                 </button>
               </div>
             );
--- client/src/components/admin/list/UsersList.jsx
+++ client/src/components/admin/list/UsersList.jsx
@@ -66,13 +66,13 @@ export default function UserList() {
             <button
               onClick={() => dispatch(deleteUser(params.id))}
               type="button"
-              class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+              className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
             >
               Eliminar
             </button>
             <button
               type="button"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
             >
               Editar
             </button>
